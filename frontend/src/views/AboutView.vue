639522eaa86ebee2cc8de34d2a66dfa89837ce77
coco <kokotenrico@gmail.com>
2022-05-21 09:16:17 +0200

    Set up frontend

---
+++ frontend/src/views/AboutView.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="about">
+    <router-link to="/"><h1>This is a back button</h1></router-link>
+    <p>Name: {{ book.name }}</p>
+    <p>Authors: {{ book.authors }}</p>
+    <p>ISBN: {{ book.isbn }}</p>
+    <p>Pages: {{ book.numberOfPages }}</p>
+    <p>Publisher: {{ book.publisher }}</p>
+    <p>Country: {{ book.country }}</p>
+    <p>Number of characters: {{ book.characters.length }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "AboutView",
+  props: ["bookUrl"],
+  data: () => ({
+    book: {},
+  }),
+  async mounted() {
+    console.log(this.bookUrl);
+    const response = await fetch(`${this.bookUrl}`);
+    const data = await response.json();
+    this.book = data;
+  },
+};
+</script>
---
+++ frontend/src/views/HomeView.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="home">
+    <li v-for="book in books" :key="book.isbn" :bookUrl="book.url">
+      <router-link :to="{ name: 'about', params: { bookUrl: book.url } }">
+        {{ book.name }}, {{ book.authors[0] }}, {{ book.released }}</router-link
+      >
+    </li>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+export default {
+  name: "HomeView",
+  async mounted() {
+    const response = await fetch("https://www.anapioficeandfire.com/api/books");
+    const data = await response.json();
+    this.books = data;
+  },
+  data: () => ({
+    books: [],
+  }),
+};
+</script>
